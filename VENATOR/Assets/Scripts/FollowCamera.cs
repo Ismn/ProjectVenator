30795f7e064c33dc15d5e5ebfb7cd4a5574bd230
Ross <ross.mcintyre93@gmail.com>
2016-07-08 04:13:33 +0100

    First Push

---
+++ VENATOR/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+    private float turnSpeed = 5.0f;
+    public Transform playerShip;
+    private Vector3 offset;
+    private Vector3 offX;
+    private Vector3 offY;
+
+    // Use this for initialization
+    void Start () {
+        offset = new Vector3(playerShip.position.x, playerShip.position.y + 3.0f, playerShip.position.z - 10.0f);
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        offX = (Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset);
+        offY = (Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset);
+        //offset = (Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset);
+        transform.position = playerShip.position + offX;
+        transform.LookAt(playerShip.position);
+	}
+}
