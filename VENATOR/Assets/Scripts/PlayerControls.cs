59e8825b19b5d09a0c99714bc0a57a248e28aa40
Ross <ross.mcintyre93@gmail.com>
2016-07-15 20:29:44 +0100

    Updating Movement
    Working on Auto-levelling the ship after changing pitch/altitude

--- VENATOR/Assets/Scripts/PlayerControls.cs
+++ VENATOR/Assets/Scripts/PlayerControls.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class PlayerControls : MonoBehaviour {
 
+    TestFlight testFlight;
+
+    // Weapons Stuff
     public GameObject laserBolt;
     public Transform blasterSpawn;
     private float loadRate = 0.5f;
     public float fireRate;
 
+    // Audio
     public AudioSource blasterSound;
     public float pitchMin = 0.95f;
     public float pitchMax = 1.05f;
@@ -16,7 +20,8 @@ public class PlayerControls : MonoBehaviour {
     Rigidbody shipRBody;
 
     // Base Velocity
-    private float velocity = 100.0f;
+    private float thrustVelocity = 100.0f;
+    private float pitchVelocity = 10.0f;
 
     // Directional Inputs
     private float thrustInput;
@@ -24,28 +29,27 @@ public class PlayerControls : MonoBehaviour {
     private float strafeInput;
     private float pitchInput;
 
-
-    // Use this for initialization
+    /// Use this for initialization
     void Start ()
     {
-        blasterSound = GetComponent<AudioSource>();
+        testFlight = GetComponent<TestFlight>();
 
-        if (GetComponent<Rigidbody>())
+        blasterSound = GetComponent<AudioSource>(); // Do we have at least one audio source?
+
+        if (GetComponent<Rigidbody>()) // Check we have a rigidbody assigned to the ship.
         {
             shipRBody = GetComponent<Rigidbody>();
         }
         else
         {
-            Debug.LogError("Ship does not have an attached rBody!");
+            Debug.LogError("Ship does not have an attached rBody!"); // Else, throw out an error.
         }
-
-        //audio.pitch = Random.Range(pitchMin, pitchMax);
     }
 
-    // Update is called once per frame
+    /// Update is called once per frame
     void Update()
     {
-        blasterSound.pitch = Random.Range(pitchMin, pitchMax);
+        blasterSound.pitch = Random.Range(pitchMin, pitchMax); // Allow for a small variance in pitch every time we fire a blaster shot.
 
         // Weapon Controls
         if (Input.GetButton("LMB") && Time.time > loadRate)
@@ -55,29 +59,34 @@ public class PlayerControls : MonoBehaviour {
             blasterSound.Play();
         }
 
-        // Find the corresponding inputs
+        // Find the corresponding inputs - See Input Manager for the keybinds to the above axes.
         thrustInput = (Input.GetAxis("Thrust"));
         yawInput = (Input.GetAxis("Yaw"));
         strafeInput = (Input.GetAxis("Strafe"));
         pitchInput = (Input.GetAxis("Pitch"));
     }
 
-    // FixedUpdate is called once per physics check
+    /// FixedUpdate is called once per physics check
     void FixedUpdate ()
     {
-        // Thrust Physics
-        shipRBody.AddRelativeForce(Vector3.forward * thrustInput * velocity);
-        // Yaw Physics
-        shipRBody.AddRelativeTorque(0, yawInput, 0);
-        // Strafe Physics
-        shipRBody.AddRelativeForce(Vector3.right * strafeInput * velocity);
-        // Pitch Physics
-        shipRBody.AddRelativeForce(Vector3.up * pitchInput * velocity);
-        shipRBody.AddRelativeTorque(Vector3.left * pitchInput);
+        testFlight.Move(0, pitchInput);
+        //// Thrust Physics
+        shipRBody.AddRelativeForce(Vector3.forward * thrustInput * thrustVelocity);
+        //// Yaw Physics
+        //shipRBody.AddRelativeTorque(0, yawInput, 0);
+        //// Strafe Physics
+        //shipRBody.AddRelativeForce(Vector3.right * strafeInput * thrustVelocity);
+        //// Pitch Physics
+        //shipRBody.AddRelativeForce(Vector3.up * pitchInput * pitchVelocity);
+        //shipRBody.AddRelativeTorque(Vector3.left * pitchInput * Time.deltaTime * pitchVelocity);
     }
 
     void Tracker()
     {
-        Debug.Log(shipRBody.velocity.magnitude);
+        //Debug.Log(shipRBody.velocity.magnitude);
+        //Debug.Log(thrustInput);
+        //Debug.Log(yawInput);
+        //Debug.Log(strafeInput);
+        //Debug.Log(pitchInput);
     }
 }
--- VENATOR/Assets/Scripts/Projectiles.cs
+++ VENATOR/Assets/Scripts/Projectiles.cs
@@ -6,6 +6,7 @@ public class Projectiles : MonoBehaviour {
     Rigidbody rbody;
     public float speed;
 
+    /// Use this for initialization
     void Start()
     {
         if (GetComponent<Rigidbody>())
